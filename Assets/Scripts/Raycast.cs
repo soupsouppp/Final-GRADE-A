c6bf74f35105fdcfab7668426a2cd1d8e5b5e811
soup <shawnazy02@gmail.com>
2022-12-08 00:15:17 -0800

    1

--- Assets/Scripts/Raycast.cs
+++ Assets/Scripts/Raycast.cs
@@ -2,7 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Unity.UI;
-
+using TMPro;
+using JetBrains.Annotations;
 
 public class Raycast : MonoBehaviour
 {
@@ -12,16 +13,22 @@ public class Raycast : MonoBehaviour
     private bool ChatOpened;
     public bool inChatRange;
 
+    public TextMeshProUGUI highlightText;
+    public GameObject Textbox;
+
 
     void Start()
     {
         ChatOpened = false;
+        highlightText = Textbox.GetComponent<TextMeshProUGUI>();
 
     }
 
 
     public void Update()
     {
+        Textbox.SetActive(false);
+
         //freeze player movement and inventory while chatting
         PlayerController playerController = player.GetComponent<PlayerController>();
         Inventory inventory = player.GetComponent<Inventory>();
@@ -34,6 +41,8 @@ public class Raycast : MonoBehaviour
 
             if (hit.transform.tag == "Item")
             {
+                Textbox.SetActive(true);
+                highlightText.text = "E to pick up";
                 inventory.inRange = true;
                 //Debug.Log("Raycast in range");
 
@@ -41,17 +50,22 @@ public class Raycast : MonoBehaviour
             else
             {
                 inventory.inRange = false;
+                Textbox.SetActive(false);
             }
 
 
             if (hit.transform.tag == "NPC")
             {
                 inChatRange = true;
+                Textbox.SetActive(true);
+                highlightText.text = "F to chat";
+
 
             }
             else
             {
                 inChatRange = false;
+                //Textbox.SetActive(false);
             }
 
 
