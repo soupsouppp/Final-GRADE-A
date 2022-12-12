34fc57ccd16709196371ca1f89bff088188dd74a
soup <shawnazy02@gmail.com>
2022-12-11 20:35:05 -0800

    1

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,12 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
+
 
 public class Enemy : MonoBehaviour
 {
 
     public float health;
+    public GameObject player;
 
+    public float Noticedistance;
+
+    private NavMeshAgent navMesh;
 
 
     public void TakeDmg(float amount)
@@ -28,13 +34,23 @@ public class Enemy : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        navMesh = GetComponent<NavMeshAgent>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float distance = Vector3.Distance(transform.position, player.transform.position);
+
+        if (distance < Noticedistance)
+        {
+            Vector3 dirToPlayer = transform.position - player.transform.position;
+
+            Vector3 newPos = transform.position - dirToPlayer;
+
+            navMesh.SetDestination(newPos);
+        }
     }
 
 
